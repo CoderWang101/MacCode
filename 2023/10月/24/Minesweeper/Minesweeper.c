39139500bb5f797cefd0a4d0146bf4edf35135cc
work-hard-to-learn-c <3132763600@qq.com>
2023-10-25 22:58:53 +0800

    10-25

--- 2023/10月/24/Minesweeper/Minesweeper.c
+++ 2023/10月/24/Minesweeper/Minesweeper.c
@@ -1,6 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
 #include <stdlib.h>
  
@@ -12,15 +10,18 @@
  
 //初始化
 void InitBoard(char board[ROWS][COLS], int rows, int cols, char set);
+
 //打印
 void DisplayBoard(char board[ROWS][COLS],int row,int col);
+
 //布置雷
 void SetMine(char mine[ROWS][COLS], int row, int col);
+
 //排查雷
 void FindMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col);
+
 // 清除排查雷周围五雷处
 void expand(char mine[ROWS][COLS], char show[ROWS][COLS], int x, int y, int* win);
-
 
 
 void InitBoard(char board[ROWS][COLS], int rows, int cols, char set)
