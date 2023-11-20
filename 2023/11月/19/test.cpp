0f53fe279666addf03234118985bd5a3ff97640f
work-hard-to-learn-c <3132763600@qq.com>
2023-11-20 18:15:04 +0800

    11-20

--- 2023/11月/19/test.cpp
+++ 2023/11月/19/test.cpp
@@ -1,58 +1,58 @@
+// // #include <iostream>
+// // using namespace std;
+
+// // class Solution
+// // {
+// //     int countA[26]={0};
+
+// //     for(auto ch : s)
+// //     {
+// //         countA[ch-'a']++;
+// //     }
+
+// //     for (int i = 0; i < s.size(); i++)
+// //     {
+// //         if (countA[s[i]-'a']==1)
+// //         {
+// //             return i;
+// //         }
+
+// //     }
+// //     return -1;
+// // };
+
 // #include <iostream>
-// using namespace std;
 
 // class Solution
 // {
-//     int countA[26]={0};
-
-//     for(auto ch : s)
+// public:
+//     int firstUniqueCharIndex(const std::string &s)
 //     {
-//         countA[ch-'a']++;
-//     }
+//         int countA[26] = {0};
 
-//     for (int i = 0; i < s.size(); i++)
-//     {
-//         if (countA[s[i]-'a']==1)
+//         for (auto ch : s)
 //         {
-//             return i;
+//             countA[ch - 'a']++;
 //         }
 
+//         for (int i = 0; i < s.size(); i++)
+//         {
+//             if (countA[s[i] - 'a'] == 1)
+//             {
+//                 return i;
+//             }
+//         }
+
+//         return -1;
 //     }
-//     return -1;
 // };
 
-#include <iostream>
+// int main()
+// {
+//     Solution solution;
+//     std::string s = "lleeticode";
+//     int index = solution.firstUniqueCharIndex(s);
+//     std::cout << "First unique character index: " << index << std::endl;
 
-class Solution
-{
-public:
-    int firstUniqueCharIndex(const std::string &s)
-    {
-        int countA[26] = {0};
-
-        for (auto ch : s)
-        {
-            countA[ch - 'a']++;
-        }
-
-        for (int i = 0; i < s.size(); i++)
-        {
-            if (countA[s[i] - 'a'] == 1)
-            {
-                return i;
-            }
-        }
-
-        return -1;
-    }
-};
-
-int main()
-{
-    Solution solution;
-    std::string s = "lleeticode";
-    int index = solution.firstUniqueCharIndex(s);
-    std::cout << "First unique character index: " << index << std::endl;
-
-    return 0;
-}
+//     return 0;
+// }
