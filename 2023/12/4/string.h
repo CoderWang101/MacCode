3cd82b0c85bd9d9c4809106985a665fac0eb561e
work-hard-to-learn-c <3132763600@qq.com>
2023-12-06 13:12:59 +0800

    12-6

--- 2023/12/4/string.h
+++ 2023/12/4/string.h
@@ -11,46 +11,21 @@ namespace wzf
         char *_str;
         size_t _size;
         size_t _capacity;
-
         static size_t npos;
 
     public:
-        typedef char *iterator;
-        typedef const char *const_iterator;
-
-        iterator begin()
-        {
-            return _str;
-        }
-
-        iterator end()
-        {
-            return _str + _size;
-        }
-
-        iterator begin()const
-        {
-            return _str;
-        }
-
-        iterator end()const
-        {
-            return _str + _size;
-        }
-
         string(const char *str = "")
             : _size(strlen(str))
         {
             _capacity = _size == 0 ? 3 : _size;
-            _str = new char[_capacity + 1];
+            _str = new char[_size + 1];
             strcpy(_str, str);
         }
 
-        // 拷贝构造
         string(const string &s)
             : _size(s._size), _capacity(s._capacity)
         {
-            _str = new char[_capacity + 1];
+            _str = new char[s._capacity];
             strcpy(_str, s._str);
         }
 
@@ -61,43 +36,41 @@ namespace wzf
             _capacity = _size = 0;
         }
 
-        const char *c_str()
-        {
-            return _str;
-        }
-
-        size_t size()const
-        {
-            return _size;
-        }
-
-        size_t capacity()const
-        {
-            return _capacity;
-        }
-
-        char &operator[](size_t pos)
-        {
-            return _str[pos];
-        }
-
-        const char &operator[](size_t pos) const
-        {
-            return _str[pos];
-        }
-
-        string &operator=(const string &s)
+        string &operator=(const string s)
         {
             if (this != &s)
             {
                 char *tmp = new char[s._capacity + 1];
                 strcpy(tmp, s._str);
-                delete _str;
+                delete[] _str;
                 _str = tmp;
                 _capacity = s._capacity;
                 _size = s._size;
             }
-            return *this;
+        }
+
+        const char *c_str()
+        {
+            return _str;
+        }
+        size_t size() const
+        {
+            return _size;
+        }
+        size_t capacity() const
+        {
+            return _capacity;
+        }
+        char &operator[](size_t pos)
+        {
+            assert(pos <= _size);
+            return _str[pos];
+        }
+
+        const char &operator[](size_t pos) const
+        {
+            assert(pos <= _size);
+            return _str[pos];
         }
 
         bool operator<(const string &s) const
@@ -110,26 +83,26 @@ namespace wzf
         }
         bool operator<=(const string &s) const
         {
-            return *this < s || *this == s;
+            return *this < s._str || *this == s._str;
         }
         bool operator>(const string &s) const
         {
-            return !(*this <= s);
+            return !(*this <= s._str);
         }
         bool operator>=(const string &s) const
         {
-            return !(*this < s);
+            return !(*this < s._str);
         }
         bool operator!=(const string &s) const
         {
-            return !(*this == s);
+            return !(*this == s._str);
         }
+
         string &operator+=(char ch)
         {
             push_back(ch);
             return *this;
         }
-
         string &operator+=(const char *str)
         {
             append(str);
@@ -151,49 +124,38 @@ namespace wzf
         string &insert(size_t pos, char ch)
         {
             assert(pos <= _size);
-
-            // 检查空间是否足够
             if (_size + 1 > _capacity)
             {
                 reserve(2 * _capacity);
             }
 
-            // 挪动数据
             size_t end = _size + 1;
-            while (pos < end)
+            while (end > pos)
             {
                 _str[end] = _str[end - 1];
-                end--;
+                --end;
             }
-
-            // 赋值
             _str[pos] = ch;
             ++_size;
-
             return *this;
         }
-
         string &insert(size_t pos, const char *str)
         {
             assert(pos <= _size);
             size_t len = strlen(str);
-
             if (_size + len > _capacity)
             {
                 reserve(_size + len);
             }
 
-            // 挪数据
-            size_t end = _size + len;
+            size_t end = len + _size;
             while (end > pos + len - 1)
             {
                 _str[end - len] = _str[end];
                 --end;
             }
-            // 拷贝字符串
             strncpy(_str + pos, str, end);
             _size += len;
-
             return *this;
         }
 
@@ -209,11 +171,11 @@ namespace wzf
 
         string &erase(size_t pos, size_t len = npos)
         {
-            assert(pos < _size);
+            assert(pos <= _size);
 
-            if (pos == len || pos + len >= _size)
+            if (len == pos || len + pos >= _size)
             {
-                _str[pos] = '\0';
+                _str[_size] = '\0';
                 _size = pos;
             }
             else
@@ -221,41 +183,14 @@ namespace wzf
                 strcpy(_str + pos, _str + pos + len);
                 _size -= len;
             }
-
             return *this;
         }
 
-        void resize(size_t n, char ch = '\0')
-        {
-            if (n < _size)
-            {
-                // 删除数据 -- 保留前n个
-                _size = n;
-                _str[_size] = '\0';
-            }
-            else if (n > _size) // n=_size不处理
-            {
-                if (n > _capacity)
-                {
-                    reserve(n);
-                }
-
-                size_t i = _size;
-                while (i < n) // 从size位置开始填字符
-                {
-                    _str[i] = ch;
-                    ++i;
-                }
-                _size = n;
-                _str[_size] = '\0';
-            }
-        }
-
-        size_t find(char ch, size_t pos = 0)
+        size_t fiund(char ch, size_t pos = 0)
         {
             assert(pos < _size);
 
-            for (size_t i = 0; i < _size; ++i)
+            for (size_t i = pos; i < _size; ++i)
             {
                 if (_str[i] == ch)
                 {
@@ -264,13 +199,12 @@ namespace wzf
             }
             return npos;
         }
-
-        size_t find(const char *str, size_t pos = 0)
+        size_t fiund(const char *str, size_t pos = 0)
         {
             assert(pos < _size);
 
             char *p = strstr(_str + pos, str);
-            if (p == nullptr)
+            if (p = nullptr)
             {
                 return npos;
             }
@@ -280,21 +214,12 @@ namespace wzf
             }
         }
 
-        // 交换
-        void swap(string &s)
-        {
-            std::swap(_str, s._str);
-            std::swap(_capacity, s._capacity);
-            std::swap(_size, s._size);
-        }
-
         void clear()
         {
             _str[0] = '\0';
             _size = 0;
         }
     };
-
     size_t string::npos = -1;
 
     ostream &operator<<(ostream &out, const string &s)
@@ -305,84 +230,34 @@ namespace wzf
         }
         return out;
     }
-
+    
     istream &operator>>(istream &in, string &s)
     {
         s.clear();
-        char ch = in.get(); // 获取一个字符
+
+        char ch = in.get();
         char buff[128];
         size_t i = 0;
 
         while (ch != ' ' && ch != '\n')
         {
             buff[i++] = ch;
-            if (i == 127) // 若已存满
+            if (i == 127)
             {
                 buff[127] = '\0';
                 s += buff;
-                i = 0; // 再次从buff[0]开始
+                i = 0;
             }
-
             ch = in.get();
         }
-
-        if (i != 0) // 防止循环结束还有数据没有进行增加
+        if (i != 0)
         {
             buff[i] = ch;
             buff[i + 1] = '\0';
             s += buff;
         }
-
         return in;
     }
 
-    void test_string1()
-    {
-        string s1("abcdef");
-        cout << s1.c_str() << endl;
-
-        string s2("efg");
-        string s3 = s1;
-        cout << s2.c_str() << endl;
-        cout << s3.c_str() << endl;
-        // cout<<(s1>s2)<<endl;
-        // cout<<(s1>=s2)<<endl;
-        // cout<<(s1<s2)<<endl;
-        //  cout<<(s1!=s2)<<endl;
-        // cout<<(s1<=s3)<<endl;
-        // cout<<(s1>s3)<<endl;
-        // cout<<(s1>=s3)<<endl;
-        // cout<<(s1!=s3)<<endl;
-        // cout<<(s1==s3)<<endl;
-    }
-    void test_string2()
-    {
-        string s1("abcdef");
-        cout << s1.c_str() << endl;
-
-        s1.push_back('!');
-        cout << s1.c_str() << endl;
-        s1.append("ABCDEF!");
-        cout << s1.c_str() << endl;
-        s1 += '2';
-        cout << s1.c_str() << endl;
-        s1 += "Hello!";
-        cout << s1.c_str() << endl;
-        s1.erase(2, 5);
-        cout << s1.c_str() << endl;
-        s1.erase(3, 100);
-        cout << s1.c_str() << endl;
-    }
-
-    void test_string3()
-    {
-        string s1("abcdef");
-        cout << s1.c_str() << endl;
-        s1.resize(7, 'x');
-        cout << s1.c_str() << endl;
-        string s2;
-        s2.resize(10, 'x');
-        cout << s2.c_str() << endl;
-    }
 
 }
