76b57f482892b2bb2fbf4421b49fe505a62be71e
work-hard-to-learn-c <3132763600@qq.com>
2023-12-10 14:30:26 +0800

    12-10

--- 2023/12/6/test.cpp
+++ 2023/12/6/test.cpp
@@ -4,9 +4,13 @@ using namespace std;
 
 int main()
 {
+    // 定义一个字符串s1
     string s1("12345");
+    // 输出s1
     cout<<s1<<endl;
-    s1.erase(1);
+    // 删除s1中的第二个字符
+    s1.erase(1);        
+    // 输出s1
     cout<<s1<<endl;
 
     return 0;
