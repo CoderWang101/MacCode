e2420b2dfbc514f9544642a62df1912a19844e65
work-hard-to-learn-c <3132763600@qq.com>
2024-02-06 09:44:45 +0800

    2-6

---
+++ 2024/2/5/Test.cpp
@@ -0,0 +1,8 @@
+#include "list.h"
+
+int main()
+{
+   // wzf::test_vector4();
+
+    return 0;
+}
---
+++ 2024/2/5/list.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include<assert.h>
+using namespace std;
+
+namespace wzf
+{
+    
+} // namespace wzf
