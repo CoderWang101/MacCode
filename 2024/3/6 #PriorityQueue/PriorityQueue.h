e6675d636fe49b0c4e6766bd52091bea071fd875
work-hard-to-learn-c <3132763600@qq.com>
2024-03-08 09:36:12 +0800

    3-8

--- 2024/3/6 #PriorityQueue/PriorityQueue.h
+++ 2024/3/6 #PriorityQueue/PriorityQueue.h
@@ -1,23 +1,47 @@
 #pragma once
+#include<functional>
+#include<queue>
 #include <iostream>
 using namespace std;
 #include <vector>
 
 namespace wzf
 {
-    template <class T, class Container = std::vector<T> >
+    template<class T>
+    struct less
+    {
+        bool operator()(const T& x,const T& y)
+        {
+            return x<y;
+        }
+    };
+    
+    template<class T>
+    struct greater
+    {
+        bool operator()(const T& x,const T& y)
+        {
+            return x>y;
+        }
+    };
+    
+
+    template <class T, class Container = std::vector<T> ,class Comapre=less<T> >//默认为大堆
     class priority_queue
     {
     public:
-        priority_queue();
-        ~priority_queue();
+        // priority_queue();
+        // ~priority_queue();
+
+        Comapre com;//使用仿函数
 
         void adjust_up(int child)
         {
-            int parent=(child-1)/2;
+            size_t parent=(child-1)/2;
             while (child>0)
             {
-                if (_con[child]>_con[parent])
+                //if (_con[child]>_con[parent])
+                if (com(_con[parent],_con[child]))
                 {
                     swap(_con[child],_con[parent]);
                     child=parent;
@@ -30,15 +54,16 @@ namespace wzf
 
         void adjust_down(int parent)
         {
-            int child=parent*2+1;//左(从0开始)
+            size_t child=parent*2+1;//左(从0开始)
             while (child<_con.size())
             {
-                if (_con[child]<_con[child+1])
-                {
+                //if (_con[child]<_con[child+1])
+                if (com(_con[child],_con[child+1])&& child+1<_con.size())//child+1<_con.size()防止越界
+                { 
                     ++child;   
                 }
                 
-                if(_con[parent]<_con[child])
+                if(com(_con[parent],_con[child]))
                 { 
                     swap(_con[parent],_con[child]);
                     parent=child;
@@ -60,7 +85,7 @@ namespace wzf
             //top与最后一个元素交换
             swap(_con[0],_con[_con.size()-1]);
             _con.pop_back();
-            //adjust_down(_con[0]);
+            adjust_down(0);
         }
 
         const T& top()
@@ -86,12 +111,12 @@ namespace wzf
     {
         //priority_queue<int,vector<int>,greater<int> > pq;
         priority_queue<int> pq;
-        pq.push(1);
-        pq.push(7);
-        pq.push(5);
-        pq.push(4);
         pq.push(9);
         pq.push(2);
+        pq.push(1);
+        pq.push(0);
+        pq.push(5);
+        pq.push(7);
 
         while (!pq.empty())
         {
