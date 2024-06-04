0f73c06ae5a837c6d67abcf04b4f516e3cfa6c4f
work-hard-to-learn-c <3132763600@qq.com>
2024-06-04 08:26:39 +0800

    6.4

--- 2024/5/28/test.py
+++ 2024/5/28/test.py
@@ -1,17 +1,28 @@
-import requests
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# import time
+
+# def search(word):
+#     path = r"/Users/white/Desktop/Code/python/venv/edgedriver_mac64/msedgedriver"
+#     options = webdriver.EdgeOptions()
+#     options.binary_location = path
+#     browser = webdriver.Edge(options=options)
+    
+#     browser.maximize_window()
+#     browser.get("http://www.baidu.com")
+#     input = browser.find_element(By.NAME, "word")
+#     input.send_keys(word + "\n")
+
+#     time.sleep(20)
+
+# if __name__ == '__main__':
+#     search('雷姆')
+
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
+from msedge.selenium_tools import Edge, EdgeOptions
+options = EdgeOptions()
+options.use_chromium = True
+options.binary_location = r"/Applications/Microsoft Edge.app" # 浏览器的位置
+driver = Edge(options=options, executable_path=r"/Users/white/Desktop/Code/python/venv/edgedriver_mac64/msedgedriver") # 相应的浏览器的驱动位置
 
-def search(word):
-    path = r"/Users/white/Desktop/Code/python/venv/edgedriver_mac64/msedgedriver"
-    browser = webdriver.Edge(executable_path=path)
-    browser.maximize_window()
-    browser.get("http://www.baidu.com")
-    input = browser.find_element(By.CSS_SELECTOR, 'input-container_94h2R input[name="word"]')
-    input.send_keys(word+"\n")
-    time.sleep(20)
-
-# class="input-container_94h2R">
-
- search('雷姆')
+driver.get("www.baidu.com")
